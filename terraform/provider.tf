c8c212e9075356bbb9b55fb4ca150b0e2edf5f59
Vaikunthan Paramananthasivam <pvaikunthan28@gmail.com>
2025-05-09 12:17:03 +0000

    Upload New File

---
+++ terraform/provider.tf
@@ -0,0 +1,11 @@
+# Specify required Terraform version and AWS provider
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+# Configure the AWS provider using the aws_region variable
+provider "aws" {
+  region = var.aws_region
+}
