00ceafa71ff0557f93203d145ac2c2c0666adbb0
Vaikunthan Paramananthasivam <pvaikunthan28@gmail.com>
2025-05-09 12:16:38 +0000

    Upload New File

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source         = "./modules/vpc"
+  aws_region     = var.aws_region
+  vpc_cidr       = var.vpc_cidr
+  public_subnets = var.public_subnets
+  tags           = var.common_tags
+}
+
+module "ecr" {
+  source          = "./modules/ecr"
+  repository_name = var.repository_name
+  tags            = var.common_tags
+}
+
+module "iam" {
+  source = "./modules/iam"
+  tags   = var.common_tags
+}
+
+module "ecs" {
+  source             = "./modules/ecs"
+  aws_region         = var.aws_region
+  cluster_name       = var.cluster_name
+  instance_type      = var.instance_type
+  desired_capacity   = var.desired_capacity
+  min_capacity       = var.min_capacity
+  max_capacity       = var.max_capacity
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  ecs_sg_id          = module.vpc.ecs_sg_id
+  instance_profile_arn = module.iam.instance_profile_arn
+  task_exec_role_arn = module.iam.task_exec_role_arn
+  repository_url     = module.ecr.repository_url
+  task_cpu           = var.task_cpu
+  task_memory        = var.task_memory
+  tags               = var.common_tags
+}
