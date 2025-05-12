cb1a65d7cbaa6f94f1063b2b4f7fb8f5c5050fd5
Vaikunthan Paramananthasivam <pvaikunthan28@gmail.com>
2025-05-12 01:08:59 +0000

    Update file outputs.tf

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -7,3 +7,14 @@ output "autoscaling_group_id" {
 output "task_definition_arn" {
   value = aws_ecs_task_definition.app.arn
 }
+# Export the ECS cluster name
+output "cluster_name" {
+  description = "Name of the ECS Cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+# Export the Auto Scaling Group name
+output "asg_name" {
+  description = "Name of the EC2 Auto Scaling Group backing the ECS cluster"
+  value       = aws_autoscaling_group.ecs_asg.name
+}
